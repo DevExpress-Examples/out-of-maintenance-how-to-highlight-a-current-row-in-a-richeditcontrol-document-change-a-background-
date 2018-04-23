ab117eae38e1e7f2d29712b37e7e7c5e1bd6f1bf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to highlight a current row in a RichEditControl document (change a background color)
+
+
+<p>The main idea of this approach is to get a document range that corresponds to the current document row and change a background color of this range.<br />To get this document range, we use the approach demonstrated in the following example: <br /><a href="https://www.devexpress.com/Support/Center/p/E3487">How to select a current line in the RichEditControl</a><br />Once a corresponding range is obtained, we change character properties of this range in the <strong>RichEditControl.SelectionChanged</strong> event handler.</p>
+
+<br/>
+
+
---
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,78 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.XtraRichEdit.API.Native
+Imports DevExpress.XtraRichEdit.Commands
+
+Namespace WpfApplication1
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+            richEditControl1.LoadDocument(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("testDocument.docx"), DevExpress.XtraRichEdit.DocumentFormat.OpenXml)
+			lineRange = richEditControl1.Document.CreateRange(0, 1)
+		End Sub
+
+		Private lineRange As DocumentRange = Nothing
+		Private isSelectionLocked As Boolean = False
+		Private Sub richEditControl1_SelectionChanged(ByVal sender As Object, ByVal e As EventArgs)
+			If isSelectionLocked Then
+				Return
+			End If
+			If (Not lineRange.Contains(richEditControl1.Document.CaretPosition)) Then
+				SetLineRangeFormatting(lineRange, System.Drawing.Color.Transparent)
+				lineRange = GetNewLineRange(richEditControl1.Document.CaretPosition)
+				SetLineRangeFormatting(lineRange, System.Drawing.Color.LightGray)
+			End If
+		End Sub
+
+		Private Sub SetLineRangeFormatting(ByVal currentLineRange As DocumentRange, ByVal rangeColor As System.Drawing.Color)
+			Dim cp As CharacterProperties = richEditControl1.Document.BeginUpdateCharacters(currentLineRange)
+			cp.BackColor = rangeColor
+			richEditControl1.Document.EndUpdateCharacters(cp)
+		End Sub
+
+		Private Function GetNewLineRange(ByVal caret As DocumentPosition) As DocumentRange
+			isSelectionLocked = True
+			Dim currentPosition As DocumentPosition = richEditControl1.Document.CaretPosition
+
+			Dim startOfLineCommand As New StartOfLineCommand(richEditControl1)
+			Dim endOfLineCommand As New EndOfLineCommand(richEditControl1)
+
+			startOfLineCommand.Execute()
+
+			Dim start As Integer = richEditControl1.Document.CaretPosition.ToInt()
+
+			endOfLineCommand.Execute()
+
+			Dim length As Integer = richEditControl1.Document.CaretPosition.ToInt() - start
+
+			Dim range As DocumentRange = richEditControl1.Document.CreateRange(start, length)
+			Dim range2 As DocumentRange = richEditControl1.Document.CreateRange(start, length + 1)
+
+			Dim text As String = richEditControl1.Document.GetText(range2)
+
+			richEditControl1.Document.CaretPosition = currentPosition
+			isSelectionLocked = False
+			If text.EndsWith(Environment.NewLine) Then
+				Return range2
+			Else
+				Return range
+			End If
+		End Function
+	End Class
+End Namespace
